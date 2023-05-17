fe51d15679ef9a5a7fd2f166061d3871fbfc1f85
sukhan220 <shahabuddink220@gmail.com>
2023-05-17 11:58:13 +0600

    Update script.js
    v2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { change, goCar, stop, start, setModel } from "./code/index.js";
-
+const carSpeed= document.querySelector("#carspeed");
 
 
 const car={
@@ -7,12 +7,27 @@ const car={
     speed:2,
     windowColor: "rgb(219, 188, 7)",
     bodyColor:"rgb(216, 79, 79)",
-    frontLightColor:"white",
-    frontLight:"on",
-    backLightColor:"white",
-    backLight:"on",
-    frontWheelColor:"black",
-    backWheelColor: "black",
+
+    frontLight:{
+        color:"white",
+        switch:"on"
+    },
+
+    backLight:{
+        color:"white",
+        switch:"on",
+    }, 
+
+    frontWheel:{
+        tireColor:"#2272b5",
+        bodyColor:"yellow",
+    },
+    
+    backWheel:{
+        tireColor:"#2272b5",
+        bodyColor:"yellow",
+    },
+
 
     start:function(){
         start();
@@ -41,6 +56,24 @@ const car={
 
 
 
+carSpeed.onchange=function(){
+    car.speed=carSpeed.value;
+    car.go();
+}
+
+
+
+
+
+
+
+
+
+
+
+
 car.build();
+
+
 
 
